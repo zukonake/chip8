1ec81713a8db06bb0fba8254958f1955e093eb5f
zukonake <zeukmc@gmail.com>
2017-05-06 17:25:40 +0200

    Added loading by arguments and better beep

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
+#include <iostream>
+//
 #include <chip8.hpp>
 
-int main()
+int main(int argc, char *argv[])
 {
+	if(argc != 2)
+	{
+		std::cout << "Usage: " << argv[0] << " filename\n";
+		return 0;
+	}
 	Chip8 chip8;
-	chip8.load("games/TANK");
+	chip8.load(argv[1]);
 	chip8.emulate();
 	return 0;
 }
